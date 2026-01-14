66bd7d80e79224045d5e3a828b4e115ea8dd97d8
ottonmq <53592672+ottonmq@users.noreply.github.com>
2026-01-13 18:56:51 -0600

    Actualizar create_admin.py
    crear superuser

--- create_admin.py
+++ create_admin.py
@@ -6,6 +6,19 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ConsultorioProject.settings')
 django.setup()
 
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')
-    print("Superusuario creado: admin / admin1234")
+
+# AQUÍ PON TUS DATOS REALES PARA ENTRAR
+mi_usuario = 'ottonmq'          # El nombre que tú quieras
+mi_correo = 'ottonmq@gmail.com'  # Tu correo real
+mi_clave = 'otto1979'      # Una clave que no olvides
+
+# Esta parte evita el error de duplicado
+if not User.objects.filter(username=mi_usuario).exists():
+    User.objects.create_superuser(mi_usuario, mi_correo, mi_clave)
+    print(f"¡Éxito! Usuario {mi_usuario} creado.")
+else:
+    # Si ya existe, actualizamos la clave por si acaso se te olvidó
+    user = User.objects.get(username=mi_usuario)
+    user.set_password(mi_clave)
+    user.save()
+    print(f"El usuario {mi_usuario} ya existía, pero hemos actualizado su clave.")
