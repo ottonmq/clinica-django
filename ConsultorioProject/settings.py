30f9bd36bf8a54ce470c2476d4da4805f7af60c9
ottonmq <53592672+ottonmq@users.noreply.github.com>
2026-01-13 21:02:26 -0600

    Actualizar settings.py
    database

--- ConsultorioProject/settings.py
+++ ConsultorioProject/settings.py
@@ -90,20 +90,11 @@ WSGI_APPLICATION = 'ConsultorioProject.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
-
-# Solo intentamos usar Postgres si la librería está instalada
-try:
-    import psycopg2
-    db_from_env = dj_database_url.config(conn_max_age=600)
-    if db_from_env:
-        DATABASES['default'].update(db_from_env)
-except ImportError:
-    pass # Si falla, usará SQLite y al menos la página cargará
 
 
 # Password validation
