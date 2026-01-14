f846023aa6d760208d6f731859925f2346396d46
ottonmq <53592672+ottonmq@users.noreply.github.com>
2026-01-14 15:21:31 -0600

    Actualizar views.py

--- consultorio/views.py
+++ consultorio/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.core import serializers
 from .models import Paciente
+from django.contrib.auth.decorators import login_required
+
+
 
 def dashboard(request):
     conteo_p = Paciente.objects.count()
