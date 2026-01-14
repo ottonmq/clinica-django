dfa62f5686f001fbb2d3ddcd5b02397aefc393ed
ottonmq <53592672+ottonmq@users.noreply.github.com>
2026-01-13 19:41:45 -0600

    Actualizar settings.py
    crear dirección host

--- ConsultorioProject/settings.py
+++ ConsultorioProject/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = ['tu-dominio.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://clinica-django-ptac.onrender.com', 'localhost', '127.0.0.1']
 
 
 
